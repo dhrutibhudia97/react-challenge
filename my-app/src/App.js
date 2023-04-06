01c3123f3ed75a9dc6495ead3c5691cc41e814c6
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-04-06 18:06:10 +0000

    Forms

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,26 +2,30 @@
 import "./App.css";
 // import NavBarSimple from "./components/NavBarSimple";
 
-// import Sidebar from "./components/Sidebar";
-// import NavBarForm from "./components/NavBarForm";
-// import Content from "./components/Content";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
 // import LifeCyclesCDM from "./components/LifeCyclesCDM";
 // import LifeCyclesCDU from "./components/LifeCyclesCDU";
 // import LifeCyclesCWU from "./components/LifeCyclesCWU";
-import ControlledForm from "./components/ControlledForm";
+// import ControlledForm from "./components/ControlledForm";
+// import UncontrolledForm from "./components/UncontrolledForm";
+import { SearchBar3 } from "./components/SearchBar3";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      {/* <NavBarForm />
+      {<NavBarForm />
       <Sidebar />
       <Content />
-      <LifeCyclesCDM /> */}
+      {/* LifeCyclesCDM /> */}
       {/* <LifeCyclesCDU /> */}
       {/* <LifeCyclesCWU /> */}
-      <ControlledForm />
-
+      {/* <ControlledForm /> */}
+      {/* <UncontrolledForm /> */}
+      {/* <SearchBar3 /> */}
+    
     </div>
   );
 }
