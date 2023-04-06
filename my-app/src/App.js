0ed7d3566695242a4591882e9487bc4628a025db
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-04-06 15:44:47 +0000

    add loader.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,25 @@
-import css from "./App.module.css";
+// import css from "./App.module.css";
+import "./App.css";
 // import NavBarSimple from "./components/NavBarSimple";
-import Sidebar from "./components/Sidebar";
-import NavBarForm from "./components/NavBarForm";
-import Content from "./components/Content";
-import LifeCyclesCDM from "./components/LifeCyclesCDM";
+
+// import Sidebar from "./components/Sidebar";
+// import NavBarForm from "./components/NavBarForm";
+// import Content from "./components/Content";
+// import LifeCyclesCDM from "./components/LifeCyclesCDM";
+// import LifeCyclesCDU from "./components/LifeCyclesCDU";
+import LifeCyclesCWU from "./components/LifeCyclesCWU";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <NavBarForm />
+      {/* <NavBarForm />
       <Sidebar />
       <Content />
-      <LifeCyclesCDM />
+      <LifeCyclesCDM /> */}
+      {/* <LifeCyclesCDU /> */}
+      <LifeCyclesCWU />
+
     </div>
   );
 }
