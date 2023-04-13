8ee88b7b648c52e0e14e9fe5099174a9f31c0c67
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-04-13 10:17:18 +0000

    the moddule

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,21 +11,41 @@ import Content from "./components/Content";
 // import ControlledForm from "./components/ControlledForm";
 // import UncontrolledForm from "./components/UncontrolledForm";
 import { SearchBar3 } from "./components/SearchBar3";
+import ControlledFormHooks from "./components/ControlledFormHooks";
+import UseStateWithArrays from "./components/UseStateWithArrays";
+import UseStateWithObjects from "./components/UseStateWithObjects";
+import UseEffectCounter from "./components/UseEffectCounter";
+import UseEffectCounterContainer from "./components/UseEffectCounterContainer";
+import ContentHooks from "./components/ContentHooks";
+import HTTPRequests from "./components/HTTPRequests";
+import HTTPPost from "./components/HTTPPost";
+import HTTPHooks from "./components/HTTPHooks";
+
+
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      {<NavBarForm />
+      {/* {<NavBarForm />
       <Sidebar />
-      <Content />
+      <Content /> */}
       {/* LifeCyclesCDM /> */}
       {/* <LifeCyclesCDU /> */}
       {/* <LifeCyclesCWU /> */}
       {/* <ControlledForm /> */}
       {/* <UncontrolledForm /> */}
       {/* <SearchBar3 /> */}
-    
+      {/* <ControlledFormHooks />
+      <UseStateWithArrays />  */}
+      {/* <UseStateWithObjects /> */}
+      {/* <UseEffectCounter/> */}
+      {/* <UseEffectCounterContainer /> */}
+      {/* <ContentHooks /> */}
+      {/* <HTTPRequests /> */}
+      {/* <HTTPPost /> */}
+      <HTTPHooks />
+
     </div>
   );
 }
